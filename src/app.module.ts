5d5b2f3e9440a7a587a6f4bfadbd0027aca01484
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2025-05-17 00:55:48 +0500

    cors added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PhoneModule } from './phone/phone.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/phones'), PhoneModule],
+  imports: [MongooseModule.forRoot(`mongodb+srv://boburovshukurullo:edwjKVYmMrxlzxzd
+@blog-app-cluster.t2nammr.mongodb.net/?retryWrites=true&w=majority&appName=blog-app-cluster`), PhoneModule],
   controllers: [AppController],
   providers: [AppService],
 })
